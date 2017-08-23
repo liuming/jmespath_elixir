f665057971585f14ebbadd10284eed93308591d8
Ming Liu <liuming@lmws.net>
2017-08-22 21:14:24 -0700

    simple nested object access

--- lib/jmespath.ex
+++ lib/jmespath.ex
@@ -1,17 +1,30 @@
 defmodule JMESPath do
   @moduledoc """
-  Elixir implementation of JMESPath.
-  """
-
-  @doc """
+  # Elixir implementation of JMESPath.
 
   ## Examples
 
-      iex> JMESPath.search
-      nil
+      iex> JMESPath.search("a.b.c", %{"a" => %{"b" => %{"c" => "C"}}})
+      "C"
+  """
 
+  @doc """
+    Search JSON object using expression
   """
   def search do
     nil
+  end
+
+  def search([first | others], json) do
+    search(others, json[first])
+  end
+
+  def search([], json) do
+    json
+  end
+
+  def search(expression, json) when is_binary(expression) do
+    [first | others] = expression |> String.split(~r/\s*\.\s*/)
+    search(others, json[first])
   end
 end
